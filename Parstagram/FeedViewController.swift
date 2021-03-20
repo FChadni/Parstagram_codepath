72109ff8ccdfce8c90c80047dafd0273a2ffb13d
FChadni <begumfa1@msu.edu>
2021-03-20 00:49:07 -0400

    initial commit

---
+++ Parstagram/FeedViewController.swift
@@ -0,0 +1,71 @@
+//
+//  FeedViewController.swift
+//  Parstagram
+//
+//  Created by Farjana Chadni on 3/19/21.
+//
+
+import UIKit
+import Parse
+import AlamofireImage
+
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var UITableView: UITableView!
+    var posts = [PFObject]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UITableView.delegate = self
+        UITableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = 20
+        
+        query.findObjectsInBackground {(posts, error) in
+            if posts != nil{
+                self.posts = posts!
+                self.UITableView.reloadData()
+            }
+        }
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let post = posts[indexPath.row]
+        
+        let user = post["author"] as! PFUser
+        cell.usernameLabel.text = user.username
+        
+        cell.captionLabel.text = post["caption"] as! String
+        
+        let imagefile = post["image"] as! PFFileObject
+        let urlString = imagefile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af_setImage(withURL: url)
+        
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
